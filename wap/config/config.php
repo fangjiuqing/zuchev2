8232994b3243c4f2ba10ad8fcfc23b0517601e5f
fangwei <123763712@qq.com>
2019-11-15 13:40:11 +0800

    edit ignore

--- wap/config/config.php
+++ wap/config/config.php
@@ -5,7 +5,7 @@ return [
         'type'      => 'mysql',
         'mysql'     => [
              'default'   => 'host=111.231.106.198;port=3306;db=rentcar;user=root;passwd=fang123wei;charset=utf8;profiling=1',
-             //'default'   => 'host=localhost;port=3306;db=rentcar;user=root;passwd=;charset=utf8;profiling=1',
+             ///'default'   => 'host=localhost;port=3306;db=rentcar;user=root;passwd=;charset=utf8;profiling=1',
         ],
     ],
    'CACHE_VER'  => 2,
